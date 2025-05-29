5564e77b7a9d6c415ccac9ca8c698f3f9aad00e1
BlackoutMinus <157079894+BlackoutMinus@users.noreply.github.com>
2025-05-29 20:39:51 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['El perro ', 'Mi abuela ', 'El jardinero borracho ', 'Mi cocodrilo '];
+let action = [' se comió', ' meó', ' empezó a lanzar piedras a', ' rompió'];
+let what = [' mi tarea', ' mi telefono', ' mi coche'];
+let when = [' antes de empezar las clases', ' mientras dormía', ' mientras hacía ejercicio', ' mientras almorzaba', ' mientras le rezaba a Cristo'];
+let phrasesArray = [who, action, what, when];
+let excuse = "";
+
 window.onload = function() {
   //write your code here
+  for (let i = 0; i < phrasesArray.length; i++) {
+    let currentPhrase = phrasesArray[i][Math.floor(Math.random() * phrasesArray[i].length)]
+    excuse += currentPhrase;
+  }
+
+  document.getElementById("excuse").innerHTML = excuse;
   console.log("Hello Rigo from the console!");
 };
